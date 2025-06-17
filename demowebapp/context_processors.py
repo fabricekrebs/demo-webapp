76886e174e8f81e5969fffbf5ffdab2107f16f4d
Fabrice Krebs <fabrice.krebs@microsoft.com>
2025-06-17 10:58:50 +0200

    Fix WSGIRequest error

--- demowebapp/context_processors.py
+++ demowebapp/context_processors.py
@@ -2,5 +2,5 @@ from django.conf import settings
 
 def backend_address(request):
     return {
-        'backend_address': settings.BACKEND_ADDRESS
+        'backend_address': str(settings.BACKEND_ADDRESS)
     }
