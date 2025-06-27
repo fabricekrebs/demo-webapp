df78c997cce408b1bab148ce8ec88d3108c1c031
Fabrice Krebs <fabrice.krebs@microsoft.com>
2025-06-27 07:46:02 +0200

    Inreasing the number of worker to 4 to allow parrallel processing

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Run the Django development server
-gunicorn demowebapp.wsgi --bind 0.0.0.0:8000
+gunicorn demowebapp.wsgi --bind 0.0.0.0:8000 --workers 4
